d4ff2b3a6e69a67c14fab3534fda1c7fa7fb16b4
Sofie Piszczek <sor004@edu.zealand.dk>
2025-05-13 12:25:19 +0200

    navbar full

---
+++ includes/navbar.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row justify-content-between">
+        <div class="col-3 px-4 py-3">
+            <img src="../images/lillelogogul.png" alt="Lille logo" class="img-fluid">
+        </div>
+        <div class="col-3 py-4 d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-bars fa-2xl text-yellow"></i>
+        </div>
+    </div>
+</div>
