a104ba36bf5883a5dcdd2ac806b420687807ed6a
Sofie Piszczek <sor004@edu.zealand.dk>
2025-05-21 10:26:12 +0200

    skelet og indsat gif

---
+++ 404.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Julehjælp</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/18ba232400.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-darkgreen h-100">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center text-white py-3 tal">4<span class="text-red">0</span>4</h1>
+        </div>
+        <div class="col-6 py-4 d-flex justify-content-center align-items-center">
+            <p class="text-white text fs-2 text-center"><span class="text-red">HOV</span> DU ER VIST LØBET IND I EN DRILLENISSE</p>
+        </div>
+        <div class="col-6 p-4 d-flex justify-content-center align-items-center">
+            <img src="video/nilloTrans.gif" class="img-fluid" alt="Drillenisse">
+        </div>
+        <div class="col-12 py-2">
+            <p class="text fs-2 text-white text-center">Glædelig jul!</p>
+        </div>
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <div class="col-3">
+                <img src="images/bomaerkehvid.png" alt="Bomærke" class="img-fluid">
+            </div>
+        </div>
+        <div class="col-12 d-flex justify-content-center py-4">
+            <div class="btn btn-outline-white instru ">
+                <a href="" class="text-decoration-none text-white">TILBAGE TIL FORSIDEN</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="js.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
