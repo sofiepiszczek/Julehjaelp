cf516d6e88e70df6001b707b14c15662d90a5055
Sofie Piszczek <sor004@edu.zealand.dk>
2025-05-22 14:22:17 +0200

    responsivitet på index

--- index.php
+++ index.php
@@ -58,12 +58,14 @@ require "settings/init.php";
     <div class="container-fluid">
         <div class="row">
             <!--citat m. typewriter-->
-            <div class="col-12 bg-darkgreen py-5 px-2">
-                <i class="fa-solid fa-quote-left fa-2xl py-3" style="color: #ffd300;"></i>
-                <p class="text-white text-center fs-3" id="typewriter"></p>
-                <div class="text-end"><i class="fa-solid fa-quote-right fa-2xl py-3" style="color: #ffd300;"></i></div>
-                <p class="text-center text-white fs-5 p-3"><i class="fa-solid fa-minus" style="color: #ffd300;"></i>
-                    Line, 30 år, mor til Liwa, 3 år & Arlo, 1 år.</p>
+            <div class="col-12 bg-darkgreen py-5 px-2 d-flex justify-content-center align-items-center">
+                <div class="col-12 col-md-10 col-lg-8">
+                    <i class="fa-solid fa-quote-left fa-2xl py-3" style="color: #ffd300;"></i>
+                    <p class="text-white text-center fs-3" id="typewriter"></p>
+                    <div class="text-end"><i class="fa-solid fa-quote-right fa-2xl py-3" style="color: #ffd300;"></i></div>
+                    <p class="text-center text-white fs-5 p-3"><i class="fa-solid fa-minus" style="color: #ffd300;"></i>
+                        Line, 30 år, mor til Liwa, 3 år & Arlo, 1 år.</p>
+                </div>
             </div>
 
             <!--Cards m. pakkeløsning-->
@@ -73,10 +75,10 @@ require "settings/init.php";
             <div>
                 <!-- card m. donation 150kr-->
                 <div>
-                    <div class="container col-9 position-relative mb-5">
+                    <div class="container col-9 col-md-6 d-lg-none position-relative mb-5">
                         <!-- card m. donation -->
                         <div class="card text-center">
-                            <div class="card-body d-flex flex-column align-items-end">
+                            <div class="card-body d-flex flex-column align-items-end pe-md-4">
                                 <h2 class="text-darkgreen">ET JULETRÆ</h2>
                                 <p>1 x juletræ til en børnefamilie</p>
                                 <div class="btn btn-darkgreen mx-3">
@@ -94,10 +96,10 @@ require "settings/init.php";
 
                 <!-- card m. donation 800kr-->
                 <div>
-                    <div class="container col-9 position-relative mb-5">
+                    <div class="container col-9 col-9 col-md-6 d-lg-none position-relative mb-5">
                         <!-- card m. donation -->
                         <div class="card text-center">
-                            <div class="card-body d-flex flex-column align-items-start">
+                            <div class="card-body d-flex flex-column align-items-start ps-md-4">
                                 <h2 class="text-darkgreen">EN JULEMIDDAG</h2>
                                 <p>1 x gavekort til rema1000</p>
                                 <div class="btn btn-darkgreen mx-3">
@@ -114,10 +116,10 @@ require "settings/init.php";
 
                 <!-- card m. donation 1500kr-->
                 <div>
-                    <div class="container col-9 position-relative mb-5 mt-1">
+                    <div class="container col-9 col-9 col-md-6 d-lg-none position-relative mb-5 mt-1">
                         <!-- card m. donation -->
                         <div class="card text-center">
-                            <div class="card-body d-flex flex-column align-items-end">
+                            <div class="card-body d-flex flex-column align-items-end pe-md-4">
                                 <h2 class="text-darkgreen">EN FULD JULEAFTEN</h2>
                                 <p class="text-end">1 x juletræ,<br> 1 x julemiddag, 1 x julegave</p>
                                 <div class="btn btn-darkgreen mx-3">
@@ -133,14 +135,82 @@ require "settings/init.php";
                         </div>
                     </div>
                 </div>
+                <!-- lg-skærm -->
+                <div class="container d-sm-none d-md-none d-lg-block mt-5">
+                    <div class="row">
+                        <div class="col-4">
+                            <!-- card m. donation 150kr-->
+                            <div class="container position-relative my-3">
+                                <!-- card m. donation -->
+                                <div class="card text-center pt-5">
+                                    <div class="card-body d-flex flex-column align-items-center">
+                                        <h2 class="text-darkgreen py-3">ET JULETRÆ</h2>
+                                        <p class="py-3">1 x juletræ til en børnefamilie</p>
+                                        <div class="btn btn-darkgreen mx-3">
+                                            <a href="" style="color:#ffd300">VÆLG</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Gave ikon -->
+                                <div id="" class="position-absolute top-0 start-50 translate-middle">
+                                    <img src="images/gave150.png" alt="Gave icon 150kr" class="img-fluid" style="width: 150px">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <!-- card m. donation 800kr-->
+                            <div>
+                                <div class="container position-relative my-3">
+                                    <!-- card m. donation -->
+                                    <div class="card text-center pt-5">
+                                        <div class="card-body d-flex flex-column align-items-center">
+                                            <h2 class="text-darkgreen py-3">EN JULEMIDDAG</h2>
+                                            <p class="py-3">1 x gavekort til rema1000</p>
+                                            <div class="btn btn-darkgreen mx-3">
+                                                <a href="" style="color:#ffd300">VÆLG</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Gave ikon -->
+                                    <div id="" class="position-absolute top-0 start-50 translate-middle">
+                                        <img src="images/gave800.png" alt="Gave icon 800kr" style="width: 140px">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <!-- card m. donation 1500kr-->
+                            <div>
+                                <div class="container position-relative my-3 ">
+                                    <!-- card m. donation -->
+                                    <div class="card text-center pt-5">
+                                        <div class="card-body d-flex flex-column align-items-center">
+                                            <h2 class="text-darkgreen py-3">EN FULD JULEAFTEN</h2>
+                                            <p class="text-center">1 x juletræ, 1 x julemiddag, 1 x julegave</p>
+                                            <div class="btn btn-darkgreen mx-3">
+                                                <a href="" style="color:#ffd300">VÆLG</a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- Gave ikon -->
+                                    <div id="" class="position-absolute top-0 start-50 translate-middle">
+                                        <img src="images/gave1500.png" alt="Gave icon 1500kr" class="img-fluid"
+                                             style="width: 150px">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <!--QR-kode card-->
-
             <div class="col-12 py-4">
                 <div class="container position-relative py-4">
                     <div class="row justify-content-center">
-                        <div class="col-8 card position-relative text-center">
+                        <div class="col-8 col-lg-7 card position-relative text-center">
                             <h3 class="py-3">Bestem selv</h3>
                             <p>Scan QR-koden eller på <span class="text text-red fs-5">13221</span><br>og støt valgfrit med MobilePay.</p>
                         </div>
@@ -154,7 +224,6 @@ require "settings/init.php";
                 </div>
             </div>
 
-
             <!--Nillo bog-->
             <div class="container-fluid bg-darkgreen py-4">
                 <div class="row justify-content-center align-items-center">
@@ -163,16 +232,16 @@ require "settings/init.php";
                         <p><i class="fa-solid fa-minus" style="color: #ffd300;"></i> <span class="text-yellow">en julekalender på 24 kapitler</span>
                         </p>
                     </div>
-                    <div class="col-6 p-3">
+                    <div class="col-6 col-lg-3 p-3">
                         <img src="images/Bogomslag.png" class="img-fluid" alt="Bogomslag">
                     </div>
-                    <div class="col-6 text-white py-4 fs-5">
+                    <div class="col-6 col-lg-3 text-white py-4 fs-5">
                         <p>En hjertevarm julefortælling om at <span class="text-yellow">modtage</span> og <span
                                     class="text-yellow">give videre</span>.</p>
                     </div>
-                    <div class="py-4 d-flex">
+                    <div class="col-12 py-4 d-flex">
                         <div class="col-6 d-flex justify-content-center align-items-center">
-                            <div class="btn btn-yellow text  text-center py-2 w-75">
+                            <div class="btn btn-yellow text  text-center py-2 w-75 ">
                                 <a href="" class="text-decoration-none text-darkgreen">KØB HER</a>
                             </div>
                         </div>
@@ -185,13 +254,15 @@ require "settings/init.php";
                 </div>
             </div>
 
-            <!--Citat m. tyoewriter-->
-            <div class="col-12 py-5 px-2">
-                <i class="fa-solid fa-quote-left fa-2xl py-3" style="color: #ffd300;"></i>
-                <p class="text-darkgreen text-center fs-3" id="typewriter2"></p>
-                <div class="text-end"><i class="fa-solid fa-quote-right fa-2xl py-3" style="color: #ffd300;"></i></div>
-                <p class="text-center text-darkgreen fs-5 p-3"><i class="fa-solid fa-minus" style="color: #ffd300;"></i>
-                    Mor, 39 år & 2 drenge på 4 og 7 år</p>
+            <!--Citat m. typewriter-->
+            <div class="col-12 py-5 px-2 d-flex justify-content-center align-items-center">
+                <div class="col-12 col-md-10 col-lg-8">
+                    <i class="fa-solid fa-quote-left fa-2xl py-3" style="color: #ffd300;"></i>
+                    <p class="text-darkgreen text-center fs-3" id="typewriter2"></p>
+                    <div class="text-end"><i class="fa-solid fa-quote-right fa-2xl py-3" style="color: #ffd300;"></i></div>
+                    <p class="text-center text-darkgreen fs-5 p-3"><i class="fa-solid fa-minus" style="color: #ffd300;"></i>
+                        Mor, 39 år & 2 drenge på 4 og 7 år</p>
+                </div>
             </div>
             <!--Infografik-->
             <div class="col-12 container-fluid py-4">
@@ -204,7 +275,7 @@ require "settings/init.php";
                     <img src="images/bowVandret.png" alt="Sløjfe" class="img-fluid pt-4 mt-1">
                 </div>
             </div>
-            <!--Tænd et lys i mørjet billede-->
+            <!--Tænd et lys i mørket billede-->
             <div class="col-12 bgimg py-5 text-center text-white d-flex flex-column justify-content-center align-items-center">
                 <h2 class="mt-3">TÆND ET LYS I MØRKET</h2>
                 <p><i class="fa-solid fa-minus" style="color: #ffd300;"></i> støt en familie i dag</p>
