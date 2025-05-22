4d1dc11e5ccd5bef883798995f26653ee98851fa
Sofie Piszczek <sor004@edu.zealand.dk>
2025-05-22 10:45:26 +0200

    var -> let

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    var i = 0;
-    var txt = 'Hvis jeg får tildelt Julehjælp, så kan jeg holde en klassisk jul ' +
-        'for mig og mine børn, en jul som jeg virkelig gerne vil give dem. En jul, hvor der måske er ' +
-        'flæskesteg og lidt ekstra lækre ting, som hører sig til. Måske noget chokolade.';
-    var speed = 50;
+    let i = 0;
+    let txt = 'Hvis jeg får tildelt Julehjælp, så kan jeg holde en klassisk jul ' +
+        'hvor der måske er flæskesteg og lidt ekstra lækre ting. Måske noget chokolade.' ;
+    let speed = 50;
 
     function typeWriter() {
         if (i < txt.length) {
@@ -29,12 +28,12 @@ document.addEventListener("DOMContentLoaded", function() {
     observer.observe(typewriterElement);
 });
 document.addEventListener("DOMContentLoaded", function() {
-    var i = 0;
-    var txt = 'Jeres hjælp til vores lille familie, mine tre sønner og mig, betyder, ' +
+    let i = 0;
+    let txt = 'Jeres hjælp til vores lille familie, mine tre sønner og mig, betyder, ' +
         'at vi kan få en julemiddag med alt det, der hører til, et lille juletræ, ' +
         'og der er en pakke under træet til hver af mine skønne drenge. ' +
         'Jeres julehjælp er det, der gør det muligt at holde jul for mine drenge.'
-    var speed = 50;
+    let speed = 50;
     function typeWriter2() {
         if (i < txt.length) {
             document.getElementById("typewriter2").innerHTML += txt.charAt(i);
