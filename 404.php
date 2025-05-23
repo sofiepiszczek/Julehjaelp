a891e7c9d9dccfd9c4cb7dbc3e82289e7ba41b21
Sofie Piszczek <sor004@edu.zealand.dk>
2025-05-23 09:52:01 +0200

    respons

--- 404.php
+++ 404.php
@@ -24,27 +24,29 @@ require "settings/init.php";
 
 <body class="bg-darkgreen h-100">
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            <h1 class="text-center text-white py-3 tal">4<span class="text-red">0</span>4</h1>
+    <div class="row justify-content-center align-items-center">
+        <div class="col-12 ">
+            <h1 class="text-center text-white pb-3 tal">4<span class="text-red">0</span>4</h1>
         </div>
-        <div class="col-6 py-4 d-flex justify-content-center align-items-center">
+        <div class="col-6 col-md-4 col-lg-3 py-4 d-flex justify-content-center align-items-center">
             <p class="text-white text fs-2 text-center"><span class="text-red">HOV</span> DU ER VIST LØBET IND I EN DRILLENISSE</p>
         </div>
-        <div class="col-6 p-4 d-flex justify-content-center align-items-center">
+        <div class="col-6 col-md-4 col-lg-3 p-4 d-flex justify-content-center align-items-center">
             <img src="video/nilloTrans.gif" class="img-fluid" alt="Drillenisse">
         </div>
-        <div class="col-12 py-2">
+        <div class="col-lg-3">
+        <div class="col-12 py-2 py-lg-4">
             <p class="text fs-2 text-white text-center">Glædelig jul!</p>
         </div>
         <div class="col-12 d-flex justify-content-center align-items-center">
-            <div class="col-3">
+            <div class="col-3 col-lg-10 text-center">
                 <img src="images/bomaerkehvid.png" alt="Bomærke" class="img-fluid">
             </div>
         </div>
+        </div>
         <div class="col-12 d-flex justify-content-center py-4">
-            <div class="btn btn-outline-white instru ">
-                <a href="" class="text-decoration-none text-white">TILBAGE TIL FORSIDEN</a>
+            <div class="btn btn-outline-white instru p-lg-2">
+                <a href="" class="text-decoration-none text-white fs">TILBAGE TIL FORSIDEN</a>
             </div>
         </div>
     </div>
