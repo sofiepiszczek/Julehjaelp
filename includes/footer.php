2ddded2778c174e103366f253f3fe1610605e4ee
emiliekolthoff <emk006@edu.zealand.dk>
2025-05-23 10:35:28 +0200

    footer rettet og indsat det rigtige stempel

--- includes/footer.php
+++ includes/footer.php
@@ -23,36 +23,50 @@
     <div class="container pt-4">
         <div class="row">
             <div class="col">
-                <p class="text-center"><b>Dansk Folkehjælp</b></p>
+                <h5 class="text-center instru mb-2"><b>Dansk Folkehjælp</b></h5>
+                <div class="pb-3">
                 <p>Brovejen 4</p>
                 <p>4800 Nykøbing Falster</p>
+                </div>
+
+                <div class="pb-3">
                 <p>Telefon: +45 70 220 230</p>
                 <p>Mail: post@folkehjaelp.dk</p>
+                </div>
+
+                <div class="pb-3">
                 <p>CVR: 20553413</p>
                 <p>P-Nummer: 1001525401</p>
+                </div>
+
                 <div class="container d-flex justify-content-evenly">
-                    <div class="col">
-                        <i class="fa-brands fa-square-facebook fa-2xl text-darkgreen"><a
+                    <div class="row g-2">
+                    <div class="col-6 pt-3">
+                        <i class="SoMeIcon fa-brands fa-square-facebook text-darkgreen "><a
                                     href="https://www.facebook.com/profile.php?id=61568307530661"
                                     target="_blank"></a></i>
-                        <i class="fa-brands fa-square-instagram fa-2xl text-darkgreen"><a
+                    </div>
+                    <div class="col-6 pt-3">
+                        <i class="SoMeIcon fa-brands fa-square-instagram text-darkgreen"><a
                                     href="https://www.instagram.com/dansk_folkehjaelp/" target="_blank"></a></i>
                     </div>
 
-
-                    <div class="col">
-                        <i class="fa-brands fa-youtube fa-2xl text-darkgreen"><a
+                    <div class="col-6 pt-2">
+                        <i class="SoMeIcon fa-brands fa-youtube text-darkgreen"><a
                                     href="https://www.youtube.com/channel/UCuCJhyfSBQQPhOD6Ztyj_bw"
                                     target="_blank"></a></i>
-                        <i class="fa-brands fa-linkedin fa-2xl text-darkgreen"><a
+                    </div>
+                    <div class="col-6 pt-2">
+                        <i class="SoMeIcon fa-brands fa-linkedin text-darkgreen"><a
                                     href="https://www.linkedin.com/company/danskfolkehjaelp/"
                                     target="_blank"></a></i>
+                    </div>
                     </div>
                 </div>
 
             </div>
             <div class="col">
-                <p class="text-center"><b>Åbningstider</b></p>
+                <h5 class="text-center instru mb-2"><b>Åbningstider</b></h5>
                 <p><b>Telefontid:</b></p>
                 <p>Mandag-Torsdag:</p>
                 <p>09:00-12:00 & 13:00-16:00</p>
@@ -63,8 +77,8 @@
                 <p>09:00-12:00 & 13:00-16:00</p>
                 <p>Fredag holdes der lukket.</p>
 
-                <div class="col">
-                    <img src="/images/lillelogo.png" alt="lille logo">
+                <div class="col text-center">
+                    <img src="/images/StempelGreen.png" alt="lille logo" class="w-50">
                 </div>
             </div>
         </div>
