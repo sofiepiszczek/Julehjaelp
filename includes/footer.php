50495f9516e842420abc33c41003ab0e34c8762a
louise <asfkls@mgrklm.dk>
2025-05-23 08:32:31 +0200

    basic footer added

---
+++ includes/footer.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Julehjælp</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/18ba232400.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+</body>
+
+<footer>
+    <div class="container pt-4">
+        <div class="row">
+            <div class="col">
+                <p class="text-center"><b>Dansk Folkehjælp</b></p>
+                <p>Brovejen 4</p>
+                <p>4800 Nykøbing Falster</p>
+                <p>Telefon: +45 70 220 230</p>
+                <p>Mail: post@folkehjaelp.dk</p>
+                <p>CVR: 20553413</p>
+                <p>P-Nummer: 1001525401</p>
+                <div class="container d-flex justify-content-evenly">
+                    <div class="col">
+                        <i class="fa-brands fa-square-facebook fa-2xl text-darkgreen"><a
+                                    href="https://www.facebook.com/profile.php?id=61568307530661"
+                                    target="_blank"></a></i>
+                        <i class="fa-brands fa-square-instagram fa-2xl text-darkgreen"><a
+                                    href="https://www.instagram.com/dansk_folkehjaelp/" target="_blank"></a></i>
+                    </div>
+
+
+                    <div class="col">
+                        <i class="fa-brands fa-youtube fa-2xl text-darkgreen"><a
+                                    href="https://www.youtube.com/channel/UCuCJhyfSBQQPhOD6Ztyj_bw"
+                                    target="_blank"></a></i>
+                        <i class="fa-brands fa-linkedin fa-2xl text-darkgreen"><a
+                                    href="https://www.linkedin.com/company/danskfolkehjaelp/"
+                                    target="_blank"></a></i>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col">
+                <p class="text-center"><b>Åbningstider</b></p>
+                <p><b>Telefontid:</b></p>
+                <p>Mandag-Torsdag:</p>
+                <p>09:00-12:00 & 13:00-16:00</p>
+                <p>Fredag:</p>
+                <p>09:00-12:00</p>
+                <p><b>Kontortid:</b></p>
+                <p>Mandag-Torsdag:</p>
+                <p>09:00-12:00 & 13:00-16:00</p>
+                <p>Fredag holdes der lukket.</p>
+
+                <div class="col">
+                    <img src="/images/lillelogo.png" alt="lille logo">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
