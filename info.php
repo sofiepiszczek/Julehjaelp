38012ab19d3ed0fb2e54b6f3dc744709d3543477
Sofie Piszczek <sor004@edu.zealand.dk>
2025-05-26 08:05:05 +0200

    respons, og ekstra slut tekst

--- info.php
+++ info.php
@@ -24,7 +24,7 @@ require "settings/init.php";
 
 <body>
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center align-items-center">
         <div class="col-12 z-2">
             <?php
             include 'includes/navbar.php';
@@ -33,7 +33,7 @@ require "settings/init.php";
         <div class="col-12 py-3">
             <h1 class="text-darkgreen text-center">HVAD ER JULEHJÆLP?</h1>
         </div>
-        <div class="col-12">
+        <div class="col-12 col-lg-10">
             <p class="text-darkgreen">
                 Julen er en af vores stærkeste fælles traditioner, men for 47.000 børn og deres forældre i Danmark 
                 kan den også være årets mest udfordrende tid. Mens mange ser frem til hygge og festligheder, 
@@ -41,10 +41,10 @@ require "settings/init.php";
                 gaver, juletræ eller små overraskelser – ting, der for mange er en naturlig del af julen.
             </p>
         </div>
-        <div class="col-12 justify-content-center align-items-center py-4">
+        <div class="col-12 col-lg-10 justify-content-center align-items-center py-4">
             <img src="images/infografik1.png" alt="Infografik" class="img-fluid">
         </div>
-        <div class="col-12">
+        <div class="col-12 col-lg-6">
                 <p class="text-darkgreen">
                     Julehjælpen er en støtteordning, der giver økonomisk trængte familier mulighed for at holde jul
                     ved at hjælpe med mad, gaver og julehygge.
@@ -53,7 +53,12 @@ require "settings/init.php";
         <div class="col-12 justify-content-center align-items-center py-4">
             <img src="images/infografik2.png" alt="Infografik2" class="img-fluid">
         </div>
-        <div class="col-12 py-4 text-center">
+        <div class="col-12 col-lg-6 text-center">
+            <p class="text-darkgreen">
+                Vær med til at hjælpe <span class="text text-uppercase text-red">Julehjælpen</span> nu, og giv en gave der holder for livet:
+            </p>
+        </div>
+        <div class="col-12 py-4 mb-5 text-center">
             <div class="btn bg-darkgreen px-3 py-2">
                 <a href="" class="text text-yellow">GIV ET BIDRAG</a>
             </div>
