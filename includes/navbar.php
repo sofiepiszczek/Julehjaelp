dff5547903f035308db416b57a2125cac95b90e4
Sofie Piszczek <sor004@edu.zealand.dk>
2025-05-27 08:49:04 +0200

    indsat href

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,8 +1,8 @@
 <!-- Navigationsbar til sm skærme -->
 <div class="container d-lg-none">
     <div class="row justify-content-between">
-        <div class="col-3 px-4 py-3">
-            <a href="/index.php"><img src="../images/LogoGulTrans.png" alt="Lille logo" class="img-fluid"></a>
+        <div class="col-3 col-md-2 px-4 py-3">
+            <a href="index.php"><img src="../images/LogoGulTrans.png" alt="Lille logo" class="img-fluid"></a>
         </div>
         <div class="col-3 py-4 d-flex align-items-center justify-content-center">
             <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="fa-solid fa-bars fa-2xl text-yellow"></i></button>
@@ -14,11 +14,11 @@
                 </div>
                 <nav class="offcanvas-body text-white text-end fs-3">
                     <ul class="list-unstyled h3">
-                        <li><a href="" class="text-decoration-none p-4 text-white d-inline-block">Giv et bidrag</a></li>
-                        <li><a href="" class="text-decoration-none p-4 text-white d-inline-block">Hvad er julehjælp?</a></li>
-                        <li><a href="" class="text-decoration-none p-4 text-white d-inline-block">Årets børnebog</a></li>
-                        <li><a href="" class="text-decoration-none p-4 text-white d-inline-block">Om os</a></li>
-                        <li><a href="" class="text-decoration-none p-4 text-white d-inline-block">Kontakt</a></li>
+                        <li><a href="vælgPakke.php" class="text-decoration-none p-4 text-white d-inline-block">Giv et bidrag</a></li>
+                        <li><a href="info.php" class="text-decoration-none p-4 text-white d-inline-block">Hvad er julehjælp?</a></li>
+                        <li><a href="nillobook.php" class="text-decoration-none p-4 text-white d-inline-block">Årets børnebog</a></li>
+                        <li><a href="omos.php" class="text-decoration-none p-4 text-white d-inline-block">Om os</a></li>
+                        <li><a href="kontakt.php" class="text-decoration-none p-4 text-white d-inline-block">Kontakt</a></li>
                     </ul>
                 </nav>
 
@@ -40,19 +40,19 @@
                 <div class="">
                     <ul class="nav">
                         <li class="nav-item">
-                            <a class="nav-link text-yellow fw-bold px-3 px-md-1 text-uppercase" href="#">Giv et bidrag</a>
+                            <a class="nav-link text-yellow fw-bold px-3 px-md-3 text-uppercase" href="vælgPakke.php">Giv et <span class="text-red">bidrag</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-yellow fw-bold px-3 px-md-1 text-uppercase" href="#">Hvad er julehjælp?</a>
+                            <a class="nav-link text-yellow fw-bold px-3 px-md-3 text-uppercase" href="info.php">Julehjælpen</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-yellow fw-bold px-3 px-md-1 text-uppercase" href="#">Årets børnebog</a>
+                            <a class="nav-link text-yellow fw-bold px-3 px-md-3 text-uppercase" href="nillobook.php">Årets børnebog</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-yellow fw-bold px-3 px-md-1 text-uppercase" href="#">Om os</a>
+                            <a class="nav-link text-yellow fw-bold px-3 px-md-3 text-uppercase" href="omos.php">Om os</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-yellow fw-bold px-3 px-md-1 text-uppercase" href="#">Kontakt os</a>
+                            <a class="nav-link text-yellow fw-bold px-3 px-md-3 text-uppercase" href="kontakt.php">Kontakt os</a>
                         </li>
                     </ul>
                 </div>
