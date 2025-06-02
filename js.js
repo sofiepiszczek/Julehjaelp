b468278a1ca8ca64ab9a5b2192c454dc2df336b7
Sofie Piszczek <sor004@edu.zealand.dk>
2025-06-02 13:27:33 +0200

    slettet totop

--- js.js
+++ js.js
@@ -54,26 +54,6 @@ document.addEventListener("DOMContentLoaded", function() {
     observer.observe(typewriterElement);
 });
 
-//ToTop button: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp//
-let mybutton = document.getElementById("ToTop");
-
-//when user scrolls down 20px from the top of the document, show the button//
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-//when user clicks on the button, scroll to the top of the document//
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 //counter for money donated//
 function animateCountUp(elementId, target, duration) {
     const element = document.getElementById(elementId);
